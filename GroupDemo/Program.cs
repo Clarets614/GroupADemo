8ebd0ee830276525f351c42db3dd9948d8aeeea6
pete382 <ivan382@hotmail.com>
2024-05-14 13:11:53 -0500

    student 3

--- GroupDemo/Program.cs
+++ GroupDemo/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("Favorite Food: Cheesy Potatoes");
 
 Console.WriteLine("Ben Jones");
 Console.WriteLine("Shellfish in general");
+
+Console.WriteLine("Pedro Rueda Diaz");
+Console.WriteLine("Bread");
